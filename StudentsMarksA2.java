d0ca166af2dc49787f0804ea581500dcf2c0c26a
rmnpch <ramonhppacheco@gmail.com>
2024-07-26 15:44:55 +1000

    To do list

--- StudentsMarksA2.java
+++ StudentsMarksA2.java
@@ -1,8 +1,14 @@
-import java.util.Scanner;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+//TODO 
+//Use % notation when printing results
+//Store students details in a class/object
+//Create explicit constructor for the parent class, instead of using Java's default constructor
+//Implement getters and setters
+//Add input validation for null grades
+//Create individual methods for individual tasks and combine them as necessary
+//Refactor code to separate methods, classes, constructors, etc
+
+import java.util.*;
+import java.io.*;
 
 public class StudentsMarksA2 {
     public StudentsMarksA2() {
@@ -62,54 +68,56 @@ public class StudentsMarksA2 {
             for (int k = 0; k < name.length; k++) {
                 if (k == 0)
                     // System.out.println("Students with grades lower than " + threshold + ": ");
-                if (total[k] < threshold) {
-                    // System.out.println(name[k]);
-                }
+                    if (total[k] < threshold) {
+                        // System.out.println(name[k]);
+                    }
             }
 
-//Bubble sorting
+            // Bubble sorting
             float tempFloat = 0;
             String tempName = "";
             int tempStudentID = 0;
-            for (int a = 0; a < total.length-1; a++){
-                for (int k =0; k < total.length-1-a; k++){
-                    if(total[k]> total[k+1]){
+            for (int a = 0; a < total.length - 1; a++) {
+                for (int k = 0; k < total.length - 1 - a; k++) {
+                    if (total[k] > total[k + 1]) {
                         tempFloat = total[k];
-                        total[k]=total[k+1];
-                        total[k+1]=tempFloat;
-                        
+                        total[k] = total[k + 1];
+                        total[k + 1] = tempFloat;
+
                         tempFloat = a1[k];
-                        a1[k]=a1[k+1];
-                        a1[k+1]=tempFloat;
-                        
+                        a1[k] = a1[k + 1];
+                        a1[k + 1] = tempFloat;
+
                         tempFloat = a2[k];
-                        a2[k]=a2[k+1];
-                        a2[k+1]=tempFloat;
-                        
+                        a2[k] = a2[k + 1];
+                        a2[k + 1] = tempFloat;
+
                         tempFloat = a3[k];
-                        a3[k]=a3[k+1];
-                        a3[k+1]=tempFloat;
-                        
+                        a3[k] = a3[k + 1];
+                        a3[k + 1] = tempFloat;
+
                         tempName = name[k];
-                        name[k]=name[k+1];
-                        name[k+1]=tempName;
-                        
+                        name[k] = name[k + 1];
+                        name[k + 1] = tempName;
+
                         tempStudentID = studentID[k];
-                        studentID[k]=studentID[k+1];
-                        studentID[k+1]=tempStudentID;
+                        studentID[k] = studentID[k + 1];
+                        studentID[k + 1] = tempStudentID;
 
                     }
                 }
             }
-            for (int a = 0; a < 5; a++){
-                if (a==0) System.out.println("Students with highest marks: ");
-                System.out.println("\t"+(a+1)+": " +name[name.length-a-1]);
+            for (int a = 0; a < 5; a++) {
+                if (a == 0)
+                    System.out.println("Students with highest marks: ");
+                System.out.println("\t" + (a + 1) + ": " + name[name.length - a - 1]);
             }
-            for (int a = 0; a < 5; a++){
-                if (a==0) System.out.println("Students with lowest marks: ");
-                System.out.println("\t"+(name.length-a)+": " +name[a]);
+            for (int a = 0; a < 5; a++) {
+                if (a == 0)
+                    System.out.println("Students with lowest marks: ");
+                System.out.println("\t" + (name.length - a) + ": " + name[a]);
             }
-            
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
